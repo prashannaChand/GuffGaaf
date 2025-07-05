64c66b1febb5288f59476f7b9a4f4350499c1f4b
PrashannaChand <chandprashanna99@gmail.com>
2025-07-05 22:44:39 +0545

    changed port

--- main.py
+++ main.py
@@ -130,5 +130,5 @@ def serve_index():
 # ----- Run app on Render -----
 if __name__ == "__main__":
     import uvicorn
-    port = int(os.environ.get("PORT", 8000))
+    port = int(os.environ.get("PORT", 10000)
     uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
